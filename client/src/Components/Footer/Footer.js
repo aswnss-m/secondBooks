37369c85027394b7ef9b3d482f683cc70f49415d
aswnss-m <aswnss.m@gmail.com>
2023-05-10 16:46:49 +0530

    footer done

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,10 +4,17 @@ import "./Footer.css"
 function Footer() {
   return (
     <div className='footerContainer'>
-      <div className="footerLogo"></div>
-      <div className="footerGetInTouch"></div>
+      <div className="footerLogo text-bold">
+        2ndBooks
+      </div>
+      <div className="footerGetInTouch">
+        <h5>Connect With Us</h5>
+        <span className='material-symbols-outlined'>clock_loader_20</span>
+        <span className='material-symbols-outlined'>clock_loader_20</span>
+        <span className='material-symbols-outlined'>clock_loader_20</span>
+      </div>
       <div className="footerLinks">
-        <h2>Quick Links</h2>
+        <h5>Quick Links</h5>
         <Link to={'/'}>Home</Link>
         <Link to={'/'}>Search</Link>
         <Link to={'/'}>About Us</Link>
