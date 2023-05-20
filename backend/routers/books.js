82dc59c209360f1bdbe9d951195549e74b32a27e
aswnss-m <aswnss.m@gmail.com>
2023-05-20 07:15:04 +0530

    changed to handle file sumbmit

--- backend/routers/books.js
+++ backend/routers/books.js
@@ -1,24 +1,26 @@
-const router = require('express').Router()
-const Books = require("../models/Books.model")
+const router = require('express').Router();
+const Book = require('../models/Book.model');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // Get All
 router.route('/').get((req, res) => {
-    Book.find()
-      .populate('seller', 'name')
-      .then(books => res.json(books))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
+  Book.find()
+    .populate('seller', 'name')
+    .then(books => res.json(books))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 // Get specific
 router.route('/:id').get((req, res) => {
-    Book.findById(req.params.id)
-      .populate('seller', 'name')
-      .then(book => res.json(book))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-// Filtered
-// GET /search?title=JavaScript&subject=Computer%20Science&maxPrice=50
+  Book.findById(req.params.id)
+    .populate('seller', 'name')
+    .then(book => res.json(book))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
+// Filtered search
 router.get('/search', async (req, res) => {
   try {
     const filters = {};
@@ -31,7 +33,7 @@ router.get('/search', async (req, res) => {
       filters.title = { $regex: title, $options: 'i' }; // Case-insensitive regex matching
     }
     if (subject) {
-      filters.Subject = subject;
+      filters.subject = subject;
     }
     if (semester) {
       filters.semester = semester;
@@ -46,7 +48,7 @@ router.get('/search', async (req, res) => {
       }
     }
     if (course) {
-      filters.Course = course;
+      filters.course = course;
     }
 
     const books = await Book.find(filters).populate('seller', 'name');
@@ -57,54 +59,60 @@ router.get('/search', async (req, res) => {
   }
 });
 
-// Add
-router.route('/').post((req, res) => {
-    const { title, semester, subject, course, author, description, price, condition, seller } = req.body;
-    
-    const newBook = new Book({
-      title,
-      semester,
-      subject,
-      course,
-      author,
-      description,
-      price,
-      condition,
-      seller
-    });
-  
-    newBook.save()
-      .then(() => res.json('Book added successfully'))
-      .catch(err => res.status(400).json('Error: ' + err));
+// Add Book
+router.route('/').post(upload.single('cover'), (req, res) => {
+  const { title, semester, subject, course, author, description, price, condition, seller } = req.body;
+
+  const newBook = new Book({
+    title,
+    cover: {
+      data: req.file.buffer,
+      contentType: req.file.mimetype
+    },
+    semester,
+    subject,
+    course,
+    author,
+    description,
+    price,
+    seller
   });
-  
-// update
-router.route('/:id').put((req, res) => {
-    Book.findById(req.params.id)
-      .then(book => {
-        book.title = req.body.title;
-        book.semester = req.body.semester;
-        book.subject = req.body.subject;
-        book.course = req.body.course;
-        book.author = req.body.author;
-        book.description = req.body.description;
-        book.price = req.body.price;
-        book.condition = req.body.condition;
-        book.seller = req.body.seller;
-  
-        book.save()
-          .then(() => res.json('Book updated successfully'))
-          .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-//   delete
+
+  newBook.save()
+    .then(() => res.json('Book added successfully'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update Book
+router.route('/:id').put(upload.single('cover'), (req, res) => {
+  Book.findById(req.params.id)
+    .then(book => {
+      book.title = req.body.title;
+      book.semester = req.body.semester;
+      book.subject = req.body.subject;
+      book.course = req.body.course;
+      book.author = req.body.author;
+      book.description = req.body.description;
+      book.price = req.body.price;
+      book.seller = req.body.seller;
+
+      if (req.file) {
+        book.cover.data = req.file.buffer;
+        book.cover.contentType = req.file.mimetype;
+      }
+
+      book.save()
+        .then(() => res.json('Book updated successfully'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Delete Book
 router.route('/:id').delete((req, res) => {
-    Book.findByIdAndDelete(req.params.id)
-      .then(() => res.json('Book deleted successfully'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Book deleted successfully'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
