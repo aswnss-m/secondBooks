864ab3e50704210732ba14e0db0c9cfad1a26a56
aswnss-m <aswnss.m@gmail.com>
2023-05-23 10:10:55 +0530

    Buy Page

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { Route,Routes } from "react-router-dom";
 import Profile from "./Pages/Profile/Profile";
 import AddBook from "./Pages/AddBook/AddBook";
 import Shopping from "./Pages/Shopping/Shopping";
-// import Buy from "./Pages/Buy/Buy";
+import Buy from "./Pages/Buy/Buy";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token')?true:false);
   useEffect(()=>{
@@ -28,11 +28,19 @@ function App() {
        <Route path={"/login"} element={<LoginPage isLoggedIn={isLoggedIn} />} />
        <Route path={"/books/:id"} element={<Shopping />} />
        {
-         isLoggedIn===true && (
+         isLoggedIn?(
            <>
            <Route path={'/profile'} element = {<Profile />} />
            <Route path={'/addBook'} element = {<AddBook />} />
+           <Route path={"/buy/:bookId"} element={<Buy />} />
           </>
+        ):(
+          <>
+          <Route path={'/profile'} element = {<LoginPage isLoggedIn={isLoggedIn} />} />
+          <Route path={'/addBook'} element = {<LoginPage isLoggedIn={isLoggedIn} />} />
+          <Route path={'/buy'} element = {<LoginPage isLoggedIn={isLoggedIn} />} />
+          <Route path={'/buy/*'} element = {<LoginPage isLoggedIn={isLoggedIn} />} />
+         </>
         )
        }
       <Route path={"/*"} element={<Home />} />
