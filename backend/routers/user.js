3a1674d0622fd1e11adda84080fcd5db262691f5
aswnss-m <aswnss.m@gmail.com>
2023-05-23 10:10:45 +0530

    updateAddress Route

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -24,7 +24,35 @@ router.route('/addbook').put(async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+router.route("/updateAddress").put(async (req,res)=>{
+  try{
+    const userId = req.body.userId;
+    const address = req.body.address;
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).json({message:"User not found"});
+    }
+    user.address = address;
+    const updatedUser = await user.save();
+    res.status(200).json({message:"User updated successfully",user:updatedUser});
+  }catch(error){
+    res.status(500).json({message:"Internal server error"});
+  }
+})
+router.route('/address').get(async (req, res) => {
+  try{
+    const userId = req.body.userId;
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).json({message:"User not found"});
+    }
+    const address = user.address;
+    res.status(200).json({message:"User updated successfully",address:address});
 
+  }catch(error){
+    res.status(500).json({message:"Internal server error", error:error});
+  } 
+})
 router.route('/:id/sell').get(async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
