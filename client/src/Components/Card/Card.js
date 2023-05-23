19bdd515f9543b51e90a703da0380afddfb25aac
aswnss-m <aswnss.m@gmail.com>
2023-05-23 07:53:10 +0530

    routing and function changes

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import './Card.css';
 
-const handleBuy = () => {
-  console.log('Buy Button Clicked');
-};
-
-const handleCart = () => {
-  console.log('Cart Button Clicked');
-};
-
 function Card({
   image,
   title,
@@ -19,17 +11,21 @@ function Card({
   _id,
   smallButton = 'Buy',
   largeButton = 'Add to Cart',
-  handleSmallButton = handleBuy, // Set the default function reference
-  handleLargeButton = handleCart, // Set the default function reference
+  handleSmallButton, // Set the default function reference
+  handleLargeButton // Set the default function reference
 }) {
-
   return (
-    <div className="cardContainer" onClick={()=>{
-      window.location.href = `/books/${_id}`;
-    }}>
+    <div className="cardContainer">
       <span className="cardCourseCode">{coursecode.slice(0, 6)}..</span>
       <div className="card">
-        <img src={image} alt={title} className="cardImage" />
+        <img
+          src={image}
+          alt={title}
+          className="cardImage"
+          onClick={() => {
+            window.location.href = `/books/${_id}`;
+          }}
+        />
         <div className="cardContent">
           <span className="cardSem cardSemester">{semester.slice(0, 3)}</span>
           <span className="cardCourse">{course.slice(0, 3)}..</span>
